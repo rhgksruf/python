5340ac2f604846198a6d592f497bc029e4e0e6b8
rhgksruf <153965086+rhgksruf@users.noreply.github.com>
2023-12-16 13:48:14 +0900

    Add files via upload

---
+++ Day 1/type_test.py
@@ -0,0 +1,26 @@
+# menu = input("서브웨이에서 어떤 메뉴를 고를건가요?")
+# print(f" {menu}메뉴는 정말 맛있죠!")
+# item = input("올리브영에서 어떤 제품을 구매하실건가요?")
+# print(f" {item}제품은 피부에 참 좋죠!")
+# a = int(input("첫 번째 수를 입력하세요"))
+# b = int(input("두 번째 수를 입력하세요"))
+#
+# # int - 정수
+# # c = int(a) #문자 ->정수화
+# # d = int(b) #문자 ->정수화
+#
+#
+#
+#
+#
+# print(f" 합한 수는 {a+b}")
+# print(f" 뺀 수는 {a-b}")
+# print(f" 곱한수는 {a*b}")
+
+A = int(input("첫 번째 수를 입력하시오"))
+B = int(input("두 번째 수를 입력하시오"))
+C = int(input("세 번째 수를 입력하시오"))
+
+print(f" 합한 수는 {A+B+C}")
+print(f" 뺀 수는 {A-B-C}")
+print(f" 곱한수는 {A*B*C}")
